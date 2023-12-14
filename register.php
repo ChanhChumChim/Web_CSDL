16239ef2b4c6f2a0ef073eb961b640d0b763ac4f
ChanhChumChim <22028052@vnu.edu.vn>
2023-12-14 16:04:02 +0700

    modified:   ecommerce_website.sql 	modified:   register.php

--- register.php
+++ register.php
@@ -94,57 +94,7 @@
       <!-- end header inner -->
       <!-- end header -->
       
-      <!-- register section --> 
-         <?php
-            include('Admin/config/config.php');
-            if (isset($_POST['dangky'])) {
-               $tenkhachhang = $_POST['hovaten'];
-               $email = $_POST['email'];
-               $dienthoai = $_POST['dienthoai'];
-               $matkhau = md5($_POST['matkhau']);
-               $diachi = $_POST['diachi'];
-               $sql_dangky = mysqli_query($mysqli,"INSERT INTO register(customer_name,email,diachi,matkhau,phone_number) 
-               VALUE ('".$tenkhachhang."','".$email."','".$diachi."','".$matkhau."','".$dienthoai."')");
-               if ($sql_dangky) {
-                  echo '<p style="color:green">Đăng ký thành công !</p>';
-               }
-            }
-         ?>
-         <p>Đăng Ký Thành Viên</p>
-         <style type="text/css">
-            table.dangky tr td {
-               padding: 5px;
-            }
-         </style>
-         <form action="" method="POST">
-            <table class="dangky" border='1' width="50%" style="border-collapse: collapse;">
-               <tr>
-                  <td>Họ và tên</td>
-                  <td><input type="text" size="50" name="hovaten"></td>
-               </tr>
-               <tr>
-                  <td>Email</td>
-                  <td><input type="text" size="50" name="email"></td>
-               </tr>
-               <tr>
-                  <td>Điện thoại</td>
-                  <td><input type="text" size="50" name="dienthoai"></td>
-               </tr>
-               <tr>
-                  <td>Địa chỉ</td>
-                  <td><input type="text" size="50" name="diachi"></td>
-               </tr>
-               <tr>
-                  <td>Mật khẩu</td>
-                  <td><input type="text" size="50" name="matkhau"></td>
-               </tr>
-               <tr>
-                  <td colspan="2"><input type="submit" name="dangky" value="Đăng ký"></td>
-               </tr>
-            </table>
-         </form>  
-
-      <!-- end register section -->
+   </body>
 
       <!--  footer -->
       <footer>
